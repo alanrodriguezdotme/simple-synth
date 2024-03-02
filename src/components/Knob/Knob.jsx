c37c711f2dc6b79d5cc9738e46d225f16f3aeab6
Alan Rodriguez <info@alanrodriguez.me>
2024-03-02 09:32:19 -0800

    Adding hook for events

--- src/components/Knob/Knob.jsx
+++ src/components/Knob/Knob.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 import styles from "./Knob.module.scss";
 import PropTypes from "prop-types";
+import useEventListener from "../../utils/useEventListener";
 
 export default function Knob({ min, max, value, onChange }) {
   const [rotation, setRotation] = useState(0);
@@ -10,8 +11,6 @@ export default function Knob({ min, max, value, onChange }) {
   const handleMouseDown = useCallback((e) => {
     setIsMouseDown(true);
     setStartCoords({ x: e.pageX, y: e.pageY });
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleMouseUp, { once: true });
   }, []);
 
   const handleMouseMove = (e) => {
@@ -21,15 +20,17 @@ export default function Knob({ min, max, value, onChange }) {
       const adjustedDiff = (clientX - x) / 50;
       const degrees = (adjustedDiff * 180) / Math.PI;
       const limitedDegrees = Math.max(-135, Math.min(135, degrees));
-      console.log({ x, clientX, degrees, limitedDegrees });
       setRotation(limitedDegrees);
     }
   };
 
   const handleMouseUp = () => {
     setIsMouseDown(false);
-    document.removeEventListener("mousemove", handleMouseMove);
   };
+
+  useEventListener("mousedown", handleMouseDown);
+  useEventListener("mousemove", handleMouseMove);
+  useEventListener("mouseup", handleMouseUp);
 
   return (
     <div
